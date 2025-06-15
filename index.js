f6bf4ab8bf42923b22a108d4ba9aed028b9b1529
david <david146182003@gmail.com>
2025-06-14 22:28:25 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+
+const app =express();
+const port = 3001;
+
+app.get('/user', (req, res)=>{
+    res.send('Hello SBA')
+})
+
+app.post('/user/:userId/:name', (req, res)=>{
+    const userId = req.params.userId;
+    const userName = req.params.name;
+    let user ={
+        id : userId,
+        name : userName
+    }
+    res.json(user)
+})
+
+app.listen(port, ()=>{
+    console.log(`server is running at ${port}`)
+})
