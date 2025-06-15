ab4da19a701b1d2621cbcd153447dc9e309d548e
david <david146182003@gmail.com>
2025-06-15 04:28:43 -0400

    render user to EJs

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ app.set("view engine", "ejs")
 app.use(express.json())
 
 app.get('/', (req, res)=>{
-
-    res.render('index')
+    
+    res.render('index', {array})
 })
 
 app.post('/', (req, res)=>{
+    console.log(req.query)
+    console.log(req.params)
     res.send('Submit name')
 })
 
@@ -22,19 +24,32 @@ app.delete('/', (req, res)=>{
     res.send('Delete name')
 })
 
-app.get('/user', (req, res)=>{
-    res.render('Hello SBA')
+app.get('/user/:name/:email', (req, res)=>{
+    console.log(req.params)
+    console.log(req.query)
+
+    let user= {
+        name: req.query.firstName,
+        email: req.query.email
+    }
+    console.log(user)
+    res.render('index', {user})
+    
+})
+app.post('/user/:id/:name', (req, res)=>{
+    
+    res.render('index', {users})
 })
 
-app.post('/user/:userId/:name', (req, res)=>{
-    const userId = req.params.userId;
-    const userName = req.params.name;
-    let user ={
-        id : userId,
-        name : userName
-    }
-    res.json(user)
-})
+// app.post('/user/:userId/:name', (req, res)=>{
+//     const userId = req.params.userId;
+//     const userName = req.params.name;
+//     let user ={
+//         id : userId,
+//         name : userName
+//     }
+//     res.json(user)
+// })
 
 app.listen(port, ()=>{
     console.log(`server is running at ${port}`)
