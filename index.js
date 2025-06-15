61f1200055fdd3048596bc546ed6846031565d37
david <david146182003@gmail.com>
2025-06-15 01:32:02 -0400

    adding post

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 import express from 'express'
 
 
+
 const app =express();
-const port = 3001;
+const port = 3000;
 
 app.set("view engine", "ejs")
 
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+
+    res.render('index')
+})
+
+app.post('/', (req, res)=>{
+    res.send('Submit name')
+})
+
+app.delete('/', (req, res)=>{
+    res.send('Delete name')
+})
+
 app.get('/user', (req, res)=>{
-    res.send('Hello SBA')
+    res.render('Hello SBA')
 })
 
 app.post('/user/:userId/:name', (req, res)=>{
